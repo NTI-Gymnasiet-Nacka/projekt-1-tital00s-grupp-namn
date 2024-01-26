12eb2d3031af4e48f3262048c79628fac7ebbc6f
Niklas zimmer <niklas.zimmer@elev.ga.ntig.se>
2024-01-26 13:19:14 +0100

    database setup

---
+++ setup_database.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+conn = sqlite3.connect("./db.db")
+
+cur = conn.cursor()
+
+cur.execute(''' CREATE TABLE IF NOT EXISTS reservation 
+            (id INTEGER,
+            name TEXT,
+            amt_guests INTEGER,
+            date TEXT,
+            table_nr INTEGER)
+''')
+cur.close()
+conn.close()
+
+# Det här är ett schema för vår databas där all information 
+# sparas i ordningen ovan där id i int osv. frågor mejla niklas
