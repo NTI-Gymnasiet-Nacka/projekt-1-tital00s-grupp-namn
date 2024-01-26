419c0ad77d2edee27b087351dcdd0ed343680be9
Isak W <isak.werner@elev.ga.ntig.se>
2024-01-26 13:43:29 +0100

    dari

---
+++ reservatione.py
@@ -0,0 +1,9 @@
+class Booking():
+    def __init__(self, user_amount, user_name, user_date, table_id):
+        self.user_amount = user_amount
+        self.user_id = user_name
+        self.user_date = user_date
+        self.table_id = table_id
+        
+    
+    
