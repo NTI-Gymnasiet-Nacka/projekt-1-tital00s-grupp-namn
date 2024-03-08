f4584e0c066eb6d4985ae45e9d1d5d18aeb15692
Niklas zimmer <niklas.zimmer@elev.ga.ntig.se>
2024-03-08 12:18:30 +0100

    tkinter almost done for testing

--- tk_shell.py
+++ tk_shell.py
@@ -2,6 +2,8 @@
 import customtkinter as ctk
 import tkinter as tk
 
+
+
 # Sets the appearance mode of the application
 # "System" sets the appearance same as that of the system
 ctk.set_appearance_mode("dark")	 
@@ -10,8 +12,36 @@ ctk.set_appearance_mode("dark")
 # Supported themes: green, dark-blue, blue
 ctk.set_default_color_theme("dark-blue")
 
+
+
 # window size
 appWidth, appHeight = 900, 700
+
+class ToplevelWindow(ctk.CTkToplevel):
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.geometry("900x500")
+		self.grid_rowconfigure((0, 1, 2, 3, 4), weight=1)
+		self.grid_columnconfigure((0, 1, 2, 3, 4), weight=1)
+
+		self.label = ctk.CTkLabel(self, text="Booking Page")
+		self.label.grid(row=0, column=1, sticky="n", rowspan=1)
+
+
+	def get_info(self):
+
+		self.name_input = ctk.CTkEntry(self)
+		self.name_input.insert(0, "Enter Name")
+		self.name_input.grid(row=2, column=0, sticky="n")
+
+		self.guest_amount = ctk.CTkOptionMenu(self, values=["1", "2", "3", "4", "5", "6", "7", "8"])
+		self.guest_amount.grid(row=2, column=1, sticky="n")
+
+		# self.date_input = ctk.CTkOptionMenu(self, values=[])
+		# self.date_input.grid(row=2, column=2, sticky="n")
+
+		
+		
 
 # Create App class
 class App(ctk.CTk):
@@ -21,21 +51,54 @@ class App(ctk.CTk):
 
 		self.title("The German Cock") 
 
-		self.geometry(f"{appWidth}x{appHeight}") 
+		self.geometry(f"{appWidth}x{appHeight}")
 
-		# configure grid layout (4x4)
-		self.grid_columnconfigure(1, weight=1)
-		self.grid_columnconfigure(2, weight=0)
-		self.grid_rowconfigure((0, 1, 2), weight=1)
+		self.toplevel_window = None
 
-		self.logo_label = ctk.CTkLabel(self, text="Welcome", font=ctk.CTkFont(size=20, weight="normal"))
-		self.logo_label.grid(row=1, column=1, padx=0, pady=0)
-		self.nothing = ctk.CTkLabel(self, text=" ", font=ctk.CTkFont(size=20, weight="normal"))
+	
+	def booking_page(self):
+		if self.toplevel_window is None or not self.toplevel_window_winfo_exists():
+			self.toplevel_window = ToplevelWindow(self)
+			self.toplevel_window.get_info()
+		else:
+			self.toplevel_window.focus()
+	
+	def main_page(self):
+
+		
+
+		# configure grid layout (5x5)
+		self.grid_rowconfigure((0, 1, 2, 3, 4), weight=1)
+		self.grid_columnconfigure((0, 1, 2, 3, 4), weight=1)
+
+		self.logo_label = ctk.CTkLabel(self, text="German Cock", font=ctk.CTkFont(family="Lobster", size=20, weight="bold"))
+		self.logo_label.grid(row=2, column=2, sticky="n", rowspan=1)
+
+		self.start_button = ctk.CTkButton(self, text="Click To Book", command=self.booking_page)
+		self.start_button.grid(row=2, column=2, sticky="", rowspan=1)
+
+		
+		
+
+	
+
+
+
+
+def main():
+	app = App()
+	app.main_page()
+	app.mainloop() 
+
+		
+
+		
+		
 		
 	
 
 if __name__ == "__main__":
-	app = App()
-	app.mainloop() 
+	main()
+	
 
 
