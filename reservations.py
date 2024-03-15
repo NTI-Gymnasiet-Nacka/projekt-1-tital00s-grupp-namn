a6aa85ff47e5b9376d168f1935ce617194efe1bb
Oliver Marcusson <oliver.marcusson@elev.ga.ntig.se>
2024-03-15 12:10:30 +0100

    removing reservations in staff.py now work. added functionality in reservations.py. Updated gitignore

--- reservations.py
+++ reservations.py
@@ -1,12 +1,20 @@
 import sqlite3
+from json import loads as json_loads
 
 
 class Reservation:
     """Denhär klassen är för att boka bord och spara bokningar i databasen och för att göra förändringar i bokningar"""
 
-    def __init__(self, db, user_amount, user_name, user_date, table_id):
+    @staticmethod
+    def from_db(db, db_data: list):
+        db_data = db_data[0]
+        reservation = Reservation(
+            db, db_data[2], db_data[1], db_data[3], db_data[4], db_data[0])
+        return reservation
+
+    def __init__(self, db, user_amount, user_name, user_date, table_id, id=None):
         self.db = db
-        self.id = db.next_reservation_id()
+        self.id = db.next_reservation_id() if id == None else id
         self.user_amount = user_amount
         self.user_name = user_name
         self.user_date = user_date
